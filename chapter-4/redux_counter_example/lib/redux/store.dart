9c4edd0c5a69d2dd5c10570ecd8cac88f335a89d
marcofranco <darkmarksdoe@gmail.com>
2022-12-08 16:56:20 -0600

    Fixed store type.

--- chapter-4/redux_counter_example/lib/redux/store.dart
+++ chapter-4/redux_counter_example/lib/redux/store.dart
@@ -2,4 +2,4 @@ import 'package:redux/redux.dart';
 import 'package:redux_counter_example/counter_state.dart';
 import 'package:redux_counter_example/redux/reducer.dart';
 
-final Store<CounterState> store = Store<CounterState>(counterReducer, initialState: CounterState());
+final store = Store<CounterState>(counterReducer, initialState: CounterState());
